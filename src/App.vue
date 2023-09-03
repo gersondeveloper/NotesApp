377e1800c0ee25839a5d27b727318e041eb93a62
Gerson C Filho <gersoncfilho@mac.com>
2023-09-03 09:12:46 +0100

    card text validation implemented

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,16 @@ import { ref } from 'vue'
 const showModal = ref(false);
 const newNote = ref("");
 const noteList = ref([]);
+const errorMessage = ref("");
 
 function renderColor() {
   return "hsl(" + Math.random() * 360 + ", 100%, 75%";
 }
 
 const addNote = () => {
+  if (newNote.value.length < 10) {
+    return errorMessage.value = "a nota precisa ter pelo menos 10 caracteres";
+  }
   noteList.value.push({
     id: Math.random() * 1000000,
     text: newNote.value,
@@ -28,7 +32,8 @@ const addNote = () => {
   <main>
     <div v-if="showModal" class="overlay flex items-center">
       <div class="bg-white rounded-md p-10 flex flex-col w-1/3">
-        <textarea v-model="newNote" class="border mb-3" name="note" id="note" cols="30" rows="10"></textarea>
+        <textarea v-model.trim="newNote" class="border mb-3" name="note" id="note" cols="30" rows="10"></textarea>
+        <p class="text-red-600" v-if="errorMessage">{{ errorMessage }}</p>
         <button @click="addNote" class="bg-blue-400 hover:bg-blue-500 text-white mb-3 cursor-pointer">Add note</button>
         <button @click="showModal = false" class="bg-red-200 hover:bg-red-400 text-white cursor-pointer">Close</button>
       </div>
@@ -40,8 +45,8 @@ const addNote = () => {
           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">+</button>
       </header>
       <div class="cards-container flex flex-wrap">
-        <div v-for="card in  noteList ">
-          <div class="card" :style="{ backgroundColor: card.backgroundColor }">
+        <div v-for=" card  in   noteList  ">
+          <div class="card" :key="card.id" :style="{ backgroundColor: card.backgroundColor }">
             <p class="main-text">{{ card.text }}</p>
             <p class="date">{{ card.date }}</p>
           </div>
